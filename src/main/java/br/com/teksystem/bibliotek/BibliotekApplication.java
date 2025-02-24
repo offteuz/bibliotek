5ac333bce609b8c6e6f41720bd48a7d7163089e0
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-02-23 22:21:44 -0300

    Project creation!

---
+++ src/main/java/br/com/teksystem/bibliotek/BibliotekApplication.java
@@ -0,0 +1,13 @@
+package br.com.teksystem.bibliotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teksystem/bibliotek/BibliotekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teksystem.bibliotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
